b5f10c51aa8874a7c79ed6749caef9031b274669
egj <egj@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-01-02 20:32:49 +0000

    a test.
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@198 f7ac0389-ae0c-0410-a9db-e7a4810fae10

---
+++ focustest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import dtk, dtk.util
+import time
+import sys, os
+import logging
+
+levels = {'debug':logging.DEBUG,
+          'info' :logging.INFO,
+          'warn' :logging.WARN,
+          'error':logging.ERROR,
+          'critical':logging.CRITICAL
+          }
+if 'DTKDEBUG' in os.environ:
+    level = levels[os.environ['DTKDEBUG']]
+else:
+    level = levels['error']
+
+e = dtk.Engine(name='dtk Test App')
+e.beginLogging(file = 'log.txt', level = level)
+
+col = dtk.Rows()
+lb = dtk.ListBox()
+
+lll = []
+for i in "eins zwei drei vier fuenf".split():
+    lll.append(i)
+
+text = dtk.TextEditor()
+text.setText("This is the text")
+
+lb.setItems(lll)
+
+col.addRow(lb)
+col.addRow(text)
+
+e.setRoot(col)
+#e.setFocus(col)
+
+e.bindKey('q', e.quit)
+
+e.mainLoop()
