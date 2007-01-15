81d21351f5b4962920d0020e47cb6c7585b1dfbe
egj <egj@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-01-15 18:29:38 +0000

    added failing test for #22: trying to scroll beyond the screen height in a texteditor results in curses.error: wmove() returned ERR
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@287 f7ac0389-ae0c-0410-a9db-e7a4810fae10

---
+++ bugs/22.py
@@ -0,0 +1,46 @@
+"""
+Demonstrates TextEditor bug when text is taller than screen.
+Make sure to run this in a terminal window shorter than the
+length of the text, or, alternatively, press enter repeatedly
+to keep adding lines to the text until the error occurs.
+"""
+
+import dtk
+import logging
+
+e = dtk.Engine()
+e.beginLogging(file='log.txt', level=logging.DEBUG)
+
+l = dtk.TextEditor()
+l.setText([
+        'Lorem ipsum dolor sit amet,',
+        'consectetuer adipiscing elit.',
+        'Nullam ac elit. Nullam consectetuer ultrices elit. Morbi a sem.',
+        'Sed urna.',
+        'Nam magna erat', 'pellentesque quis', 'ultricies id',
+        'hendrerit molestie, nibh.',
+        'Cras placerat pellentesque erat.',
+        'Vestibulum fringilla,',
+        'ligula eget tristique convallis',
+        'enim',
+        'risus dignissim lacus, at pharetra velit purus vitae lectus.',
+        'Vivamus aliquet',
+        'vehicula nunc. Nulla commodo ligula eu felis.',
+        'Duis eu sapien quis',
+        'leo posuere rutrum. Suspendisse porttitor',
+        'pulvinar elit.',
+        'In scelerisque elit a elit. Praesent accumsan.',
+        'Fusce sed lacus',
+        'in lectus varius aliquam. Proin lobortis.',
+        'Vestibulum ante ipsum primis in faucibus orci luctus et',
+        'ultrices posuere cubilia Curae; Suspendisse aliquam ultrices pede.',
+        'Suspendisse bibendum dolor vel diam. Morbi vel arcu vitae',
+        'magna fermentum interdum. Donec ornare sollicitudin enim.'
+        ])
+
+e.bindKey('esc', e.quit)
+
+e.setRoot(l)
+
+e.mainLoop()
+
