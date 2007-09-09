b33664b20afd7184adf89060f1253818484de0a3
dcrosta <dcrosta@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-09-09 21:28:21 +0000

    adapt bugNN.py files to DtkTestCases
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@320 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- test/bugs/__init__.py
+++ test/bugs/__init__.py
@@ -1,1 +1,27 @@
+import os, os.path
 
+"""
+# get a list of all the modules in this directory -- that is,
+# every file that ends in .py except __init__.py
+mod_path = os.path.dirname(os.path.join(os.getcwd(), __file__))
+modules = []
+for name in os.listdir(mod_path):
+    (base, ext) = os.path.splitext(name)
+    if ext == '.py' and (not base == '__init__'):
+        modules.append(base)
+
+
+# import into 'dtk' namespace only those things
+# with the same name as the file they appear in,
+# and then only if it begins with a capital letter
+for module in modules:
+    try:
+        exec 'from %s import %s' % (module, module)
+    except SyntaxError:
+        pass
+"""
+
+# TODO: when the other bug*.py files are adapted to DtkTestCases,
+# use the above to automatically include them in the tests run
+# by runner.py (outside this directory)
+from bug22 import BugTwentyTwo
--- test/bugs/bug22.py
+++ test/bugs/bug22.py
@@ -7,40 +7,65 @@ to keep adding lines to the text until the error occurs.
 
 import dtk
 import logging
+import sys
 
-e = dtk.Engine()
-e.beginLogging(file='log.txt', level=logging.DEBUG)
+# set up an import path to ../ so we can get the
+# DtkTestCase class
+#sys.path.insert( 1, os.path.abspath( os.path.join( os.path.dirname( __file__ ), '..' ) ) )
 
-l = dtk.TextEditor()
-l.setText([
-        'Lorem ipsum dolor sit amet,',
-        'consectetuer adipiscing elit.',
-        'Nullam ac elit. Nullam consectetuer ultrices elit. Morbi a sem.',
-        'Sed urna.',
-        'Nam magna erat', 'pellentesque quis', 'ultricies id',
-        'hendrerit molestie, nibh.',
-        'Cras placerat pellentesque erat.',
-        'Vestibulum fringilla,',
-        'ligula eget tristique convallis',
-        'enim',
-        'risus dignissim lacus, at pharetra velit purus vitae lectus.',
-        'Vivamus aliquet',
-        'vehicula nunc. Nulla commodo ligula eu felis.',
-        'Duis eu sapien quis',
-        'leo posuere rutrum. Suspendisse porttitor',
-        'pulvinar elit.',
-        'In scelerisque elit a elit. Praesent accumsan.',
-        'Fusce sed lacus',
-        'in lectus varius aliquam. Proin lobortis.',
-        'Vestibulum ante ipsum primis in faucibus orci luctus et',
-        'ultrices posuere cubilia Curae; Suspendisse aliquam ultrices pede.',
-        'Suspendisse bibendum dolor vel diam. Morbi vel arcu vitae',
-        'magna fermentum interdum. Donec ornare sollicitudin enim.'
-        ])
+from dtktest import DtkTestCase
 
-e.bindKey('esc', e.quit)
+class BugTwentyTwo(DtkTestCase):
 
-e.setRoot(l)
+    def testLongTextEditor(self):
+        """
+        TextEdtior raises an exception if you scroll down too far.
+        This test is considered to pass if it raises no exception
+        """
 
-e.mainLoop()
+        args = ['down'] * 25
+        args.append('esc')
+        self.scr.set_input(*args)
+        
+        e = dtk.Engine()
+        l = dtk.TextEditor()
+        l.setText([
+                'Lorem ipsum dolor sit amet,',
+                'consectetuer adipiscing elit.',
+                'Nullam ac elit. Nullam consectetuer ultrices elit. Morbi a sem.',
+                'Sed urna.',
+                'Nam magna erat', 'pellentesque quis', 'ultricies id',
+                'hendrerit molestie, nibh.',
+                'Cras placerat pellentesque erat.',
+                'Vestibulum fringilla,',
+                'ligula eget tristique convallis',
+                'enim',
+                'Lorem ipsum dolor sit amet,',
+                'consectetuer adipiscing elit.',
+                'Nullam ac elit. Nullam consectetuer ultrices elit. Morbi a sem.',
+                'Sed urna.',
+                'Nam magna erat', 'pellentesque quis', 'ultricies id',
+                'hendrerit molestie, nibh.',
+                'Cras placerat pellentesque erat.',
+                'Vestibulum fringilla,',
+                'ligula eget tristique convallis',
+                'enim',
+                'risus dignissim lacus, at pharetra velit purus vitae lectus.',
+                'Vivamus aliquet',
+                'vehicula nunc. Nulla commodo ligula eu felis.',
+                'Duis eu sapien quis',
+                'leo posuere rutrum. Suspendisse porttitor',
+                'pulvinar elit.',
+                'In scelerisque elit a elit. Praesent accumsan.',
+                'Fusce sed lacus',
+                'in lectus varius aliquam. Proin lobortis.',
+                'Vestibulum ante ipsum primis in faucibus orci luctus et',
+                'ultrices posuere cubilia Curae; Suspendisse aliquam ultrices pede.',
+                'Suspendisse bibendum dolor vel diam. Morbi vel arcu vitae',
+                'magna fermentum interdum. Donec ornare sollicitudin enim.'
+                ])
+
+        e.bindKey('esc', e.quit)
+        e.setRoot(l)
+        e.mainLoop()
 
