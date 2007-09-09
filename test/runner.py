39820f29e5cde740067bba58d552c1e36d1f1a99
dcrosta <dcrosta@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-09-09 21:26:59 +0000

    extract some stuff to dtktest.py
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@319 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- test/runner.py
+++ test/runner.py
@@ -16,39 +16,12 @@ sys.path.insert( 1, os.path.abspath( os.path.join( os.path.dirname( __file__ ), 
 import curses
 import dtk
 
+# this defines the test base class and a simple test case
+# for sanity checking
+from dtktest import *
 
-class DtkTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.scr = curses.Screen(24, 80)
-        curses._scr = self.scr
-        curses.use_delay(False)
-        curses.print_screen(False)
-
-    def assertTextAt(self, y, x, text, time=None):
-        t = self.scr.get_text_at(y, x, len(text), time)
-
-        self.assertEquals(text, t, 'screen at (%d, %d) at time %g: "%s" != "%s"' % (y, x, time, text, t))
-
-
-class TestBasics(DtkTestCase):
-
-    def testHelloWorld(self):
-        self.scr.set_input('q')
-
-        class Canvas(dtk.Drawable):
-            def __init__(self):
-                super(Canvas, self).__init__()
-            def render(self):
-                self.draw('hello, world', 1, 1)
-
-        e = dtk.Engine()
-        e.setRoot(Canvas())
-        e.bindKey('q', e.quit)
-        e.mainLoop()
-
-        self.assertTextAt(1, 1, 'hello, world', 1)
-
+# get any test cases defined in bugs/
+from bugs import *
 
 if __name__ == '__main__':
     unittest.main()
