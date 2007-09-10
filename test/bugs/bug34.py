8dcf9b7f2541b4b1df256558403ebbb81bcc94ae
dcrosta <dcrosta@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-09-10 15:38:09 +0000

    event order is undefined, apparently -- this test now doesn't depend on order to pass
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@324 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- test/bugs/bug34.py
+++ test/bugs/bug34.py
@@ -13,42 +13,34 @@ class BugThirtyFour(DtkTestCase):
 
         class CallableClass(object):
 
-            def __init__(self, log):
-                """
-                log should be a logging.Logger object; __call__ will
-                log arguments given to the logging object with priority
-                WARN
-                """
-                self.log = log
-
             def __call__(self, *args, **kwargs):
-                trace('called with (%s, %s)', args, kwargs)
+                trace('(%s, %s)', args, kwargs)
 
 
         class CallableWithInputKey(CallableClass):
 
             def __call__(self, _input_key, *args, **kwargs):
-                trace('called with (_input_key = %s, %s, %s)', _input_key, args, kwargs)
+                trace('(_input_key = %s, %s, %s)', _input_key, args, kwargs)
 
         class CallableWithSourceObj(CallableClass):
 
             def __call__(self, _source_obj, *args, **kwargs):
-                trace('called with (_source_obj = %s, %s, %s)', _source_obj, args, kwargs)
+                trace('(_source_obj = %s, %s, %s)', _source_obj, args, kwargs)
 
         class CallableWithBoth(CallableClass):
 
             def __call__(self, _input_key, _source_obj, *args, **kwargs):
-                trace('called with (_input_key = %s, _source_obj = %s, %s, %s)', _input_key, _source_obj, args, kwargs)
+                trace('(_input_key = %s, _source_obj = %s, %s, %s)', _input_key, _source_obj, args, kwargs)
 
         class CallableWithEventType(CallableClass):
 
             def __call__(self, _event_type, *args, **kwargs):
-                trace('called with (_event_type = %s, %s, %s)', _event_type, args, kwargs)
+                trace('(_event_type = %s, %s, %s)', _event_type, args, kwargs)
 
         class CallableWithEventTypeSource(CallableClass):
 
             def __call__(self, _event_type, _source_obj, *args, **kwargs):
-                trace('called with (_event_type = %s, _source_obj = %s, %s, %s)', _event_type, _source_obj, args, kwargs)
+                trace('(_event_type = %s, _source_obj = %s, %s, %s)', _event_type, _source_obj, args, kwargs)
 
 
         self.scr.set_input('a','b','c','d','enter','q')
@@ -61,12 +53,12 @@ class BugThirtyFour(DtkTestCase):
 
         e.setRoot(b)
 
-        mycallable = CallableClass(e.log)
-        mycallable_i = CallableWithInputKey(e.log)
-        mycallable_s = CallableWithSourceObj(e.log)
-        mycallable_is = CallableWithBoth(e.log)
-        mycallable_e = CallableWithEventType(e.log)
-        mycallable_es = CallableWithEventTypeSource(e.log)
+        mycallable = CallableClass()
+        mycallable_i = CallableWithInputKey()
+        mycallable_s = CallableWithSourceObj()
+        mycallable_is = CallableWithBoth()
+        mycallable_e = CallableWithEventType()
+        mycallable_es = CallableWithEventTypeSource()
 
         b.bindKey('a', mycallable)
         b.bindKey('b', mycallable_i)
@@ -80,16 +72,28 @@ class BugThirtyFour(DtkTestCase):
 
         e.mainLoop()
 
+
+        def map_of_counts(l):
+            m = {}
+            for elm in l:
+                m[elm] = m.get(elm, 0) + 1
+            return m
+
         expected = [
-            'called with ((), {})',
-            'called with (_input_key = b, (), {})',
-            'called with (_source_obj = Button, (), {})',
-            'called with (_input_key = d, _source_obj = Button, (), {})',
-            'called with ((), {})',
-            'called with (_event_type = clicked, (), {})',
-            'called with (_event_type = clicked, _source_obj = Button, (), {})',
-            'called with (_source_obj = Button, (), {})',
+            '((), {})',
+            '(_input_key = b, (), {})',
+            '(_source_obj = Button, (), {})',
+            '(_input_key = d, _source_obj = Button, (), {})',
+            '((), {})',
+            '(_event_type = clicked, (), {})',
+            '(_event_type = clicked, _source_obj = Button, (), {})',
+            '(_source_obj = Button, (), {})',
         ]
+
         self.assertEquals(len(expected), len(call_trace), "same # of actual calls as expected")
-        for e, a, i in zip(expected, call_trace, range(len(expected))):
-            self.assertEquals(e, a, "expected call %d" % i)
+
+        expected = map_of_counts(expected)
+        actual = map_of_counts(call_trace)
+
+        for elm in expected:
+            self.assert_(elm in actual)
