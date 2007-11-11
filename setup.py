ccef4d7fe344b59a24d1c1330931ab6fd9f42aaf
egj <egj@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2007-11-11 18:39:30 +0000

    use setuptools instead of distutils
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@331 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 import dtk
 
+version = '0.3'
 
-setup(name='dtk', version=str(dtk.__version__), packages=['dtk'])
+setup(name='dtk',
+      version=version,
+      description="A curses 'GUI' toolkit for Python programs",
+      author="Dan Crosta, Ethan Jucovy",
+      author_email="dtk-dev@lists.openplans.org",
+      url="https://trac.sccs.swarthmore.edu/dtk",
+      license="LGPLv3",
+      packages=['dtk'])
