b03b4f75e5e5e196b5bb628426e8389019d02a3e
dcrosta <dcrosta@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2008-04-11 18:20:11 +0000

    now we immediately parse special keys, and get escape right away without having to hit it twice! better than curses!
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/branches/experiments/newthreads@407 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- input.py
+++ input.py
@@ -1,7 +1,9 @@
 import threading
 import time
+import types
 import curses
 import curses.ascii
+import traceback
 
 waiter = threading.Event()
 
@@ -46,14 +48,69 @@ def curses_input(scr):
     scr.move(1,0)
     scr.addstr("blah")
 
-    while True:
-        input = scr.getch()
-        if curses.ascii.isprint(input):
-            input = chr(input)
+    lasttime = time.time()
+    line = 3
 
-        scr.move(3, 0)
+
+    input_queue = []
+    while True:
+
+        while True:
+            input = scr.getch()
+            # have to do this after we get the char
+            scr.nodelay(1)
+            input_queue.append(input)
+            if input == -1:
+                scr.nodelay(0)
+                break
+
+        km = keymap
+        i = 0
+        for char in input_queue:
+            scr.move(15 + i, 0)
+            scr.clrtoeol()
+            key = '.'.join([str(x) for x in input_queue[0:min(i+1,len(input_queue))]])
+            try:
+                scr.addstr(15 + i, 0, "type at key %s is " % key)
+                km = km[char]
+                scr.addstr(str(type(km)))
+            except Exception, e:
+                # usually this exception is OK, because
+                # it often comes from printable keys, which
+                # are not mapped in the keymap
+                break
+
+        # if input_queue is 2 long (something then a -1),
+        # then the first element is the key code for a
+        # printable character; else km should be the string
+        # name of the key found from the keymap
+        if len(input_queue) == 2 and input_queue[0] not in keymap:
+            input = input_queue[0]
+            if curses.ascii.isprint(input):
+                input = chr(input)
+
+        elif type(km) != types.StringType:
+            # throw an exception?
+            #km = str(input_queue) # for now
+            pass
+
+        else:
+            input = km
+
+        now = time.time()
+        if now - lasttime < 0.1:
+            line += 1
+        else:
+            for y in range(3, 8):
+                scr.move(y, 0)
+                scr.clrtoeol()
+            line = 3
+        lasttime = time.time()
+
+        scr.move(line, 0)
         scr.clrtoeol()
-        scr.addstr(3, 0, "input was '%s'" % input)
+        scr.addstr(line, 0, "input was '%s'" % str(input))
+        scr.addstr(8, 0, "iq was '%s'" % str(input_queue))
         if input == 'q':
             m.stop()
             break
@@ -64,10 +121,45 @@ def curses_input(scr):
         waiter.set()
         scr.addstr(11, 0, "Main notifying         ")
 
+        input_queue = []
+
 
     waiter.set()
     m.join()
+    scr.move(0, 0)
+    scr.clrtobot()
     curses.endwin()
+
+# map special keys to nicer names
+keymap = {
+    27: { -1: "esc",
+          91: { 49: { 53: { 126: { -1: "F5" } },
+                      55: { 126: { -1: "F6" } },
+                      56: { 126: { -1: "F7" } },
+                      57: { 126: { -1: "F8" } }
+                    },
+                50: { 48: { 126: { -1: "F9" } },
+                      49: { 126: { -1: "F10" } },
+                      51: { 126: { -1: "F11" } },
+                      52: { 126: { -1: "F12" } },
+                    },
+                53: { 126: { -1: "page up" } },
+                54: { 126: { -1: "page down" } },
+                65: { -1: "up" },
+                66: { -1: "down" },
+                67: { -1: "right" },
+                68: { -1: "left" },
+                70: { -1: "end" },
+                72: { -1: "home" },
+              },
+          79: { 80: { -1: "F1" },
+                81: { -1: "F2" },
+                82: { -1: "F3" },
+                83: { -1: "F4" },
+              },
+        },
+    127: { -1: "backspace" }
+    }
 
 
 def start():
