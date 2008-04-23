4799ef54dea78768468d2d9788b69d1abd71e455
egj <egj@f7ac0389-ae0c-0410-a9db-e7a4810fae10>
2008-04-23 03:41:46 +0000

    Hum-drum package maintenance:  * Update changelog for 0.3 (with new format!)  * Update links in setup.py  * Remove broken test_suite  * Update long_description to read changelog as well as readme
    This line, and those below, will be ignored--
    
    M    ChangeLog
    M    setup.py
    
    
    git-svn-id: http://svn.sccs.swarthmore.edu/dtk/trunk@425 f7ac0389-ae0c-0410-a9db-e7a4810fae10

--- setup.py
+++ setup.py
@@ -4,19 +4,16 @@ from setuptools import setup
 
 version = '0.3'
 
-f = open('README.txt')
-readme = "".join(f.readlines())
-f.close()
+readme = open('README.txt').read() + '\n\n' + open('ChangeLog').read()
 
 setup(name='dtk',
       version=version,
       description="A curses 'GUI' toolkit for Python programs",
       long_description=readme,
       author="Dan Crosta, Ethan Jucovy",
-      author_email="dtk-dev@lists.openplans.org",
-      url="https://trac.sccs.swarthmore.edu/dtk",
+      author_email="dtk-dev@sccs.swarthmore.edu",
+      url="http://trac.sccs.swarthmore.edu/dtk",
       license="LGPLv3",
       copyright="(C) 2006-2007 Dan Crosta, Ethan Jucovy",
       docformat="epytext en",
-      test_suite="test.runner",
       packages=['dtk'])
